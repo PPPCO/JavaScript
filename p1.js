186b097065cf197d3ba9e3a08705268c87574b44
Jose Francisco <pepepaco982@gmail.com>
2023-02-22 15:17:30 -0600

    jscompleto

--- p1.js
+++ p1.js
@@ -1,4 +1,10 @@
-var nombre= "paco";
-console.log(nombre);
-let nombre_apellido ='Garcia';
-console.log(`El nombre completo es: ${nombre} ${nombre_apellido}`);
+
+  const numeros = '9,8,1,6,7,6';
+function ordenarNumeros(numeros){
+    const lista = numeros.split(',').map(num => parseInt(num));
+    lista.sort((a, b) => b - a);
+    return lista.join(','); 
+}
+
+console.log(ordenarNumeros('9,8,1,6,7,6'));
+console.log(ordenarNumeros('3,6,1,8'));
